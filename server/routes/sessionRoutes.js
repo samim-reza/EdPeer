fbbeb98f43deedf4684d803fffe60339ca172698
Ismatul Islam Pranto <prantokhaan@gmail.com>
2025-05-12 03:29:53 +0600

    session

---
+++ server/routes/sessionRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { createSession } = require('../controllers/sessionController');
+const router = express.Router();
+
+router.post("/createSession", createSession);
+
+module.exports = router;
--- server/services/sessionService.js
+++ server/services/sessionService.js
@@ -5,6 +5,18 @@ const requestSession = async (title, category, description, senderId, receiverId
         if(!title || !category || !description || !tags || !duration || !inExchange){
             throw new Error("All fields are required");
         }
+        if(!senderId) senderId = null;
+        if(!receiverId) receiverId = null;
+        if(!creditExchange) creditExchange = null;
+        if(!startTime) startTime = null;
+        if(!topic) topic = null;
+        if(!exchangeTopic) exchangeTopic = null;
+        if(!inExchange) inExchange = null;
+        if(!duration) duration = null;
+        if(!description) description = null;
+        if(!tags) tags = null;
+        if(!category) category = null;
+        
         const newSession = await Session.create({
             title,
             category,
