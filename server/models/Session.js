969cdc98069ea791b9aab7c3faae03554c739f1f
MafroseMitu <mafrosemitu0409@gmail.com>
2025-05-12 02:54:27 +0600

    session table created

---
+++ server/models/Session.js
@@ -0,0 +1,60 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const Session = sequelize.define("Session", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    senderId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    receiverId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.ENUM("pending", "running", "completed"),
+        allowNull: false,
+        defaultValue: "pending"
+    },
+    inExchange: {
+        type: DataTypes.ENUM("credit", "learning"),
+        allowNull: false,
+    },
+    duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    startTime: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    creditExchange: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    topic: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    exchangeTopic: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    tags: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = Session;
