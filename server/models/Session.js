d336f85af8b1dd69f8f3fd3a622f48c05f11eabd
MafroseMitu <mafrosemitu0409@gmail.com>
2025-05-12 03:08:03 +0600

    fixed session

--- server/models/Session.js
+++ server/models/Session.js
@@ -10,10 +10,18 @@ const Session = sequelize.define("Session", {
     },
     senderId: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
     },
     receiverId: {
         type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    senderWant: {
+        type: DataTypes.ENUM("Teach", "Learn"),
+        allowNull: false,
+    },
+    category: {
+        type: DataTypes.STRING,
         allowNull: false,
     },
     status: {
